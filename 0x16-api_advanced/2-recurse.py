33cc9d6f95e11042eb6b8205f8f555d322a22685
TheKharis <reubenagbenyegah86@gmail.com>
2023-11-07 23:45:41 +0000

    Corrections to code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
+from json import loads
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
