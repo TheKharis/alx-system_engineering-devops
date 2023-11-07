25fa73370541bc59b10b83170561e5fc49e03293
TheKharis <reubenagbenyegah86@gmail.com>
2023-11-07 23:49:49 +0000

    Corrections to task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
-from json import loads
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
@@ -26,6 +25,6 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     for c in results.get("children"):
         hot_list.append(c.get("data").get("title"))
 
-        if after is not None:
-            return recurse(subreddit, hot_list, after, count)
-        return hot_list
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
